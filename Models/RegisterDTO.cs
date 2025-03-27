c4a4e6090244aa9b1e57f09b5e6a9161a27f47c8
fadel-amar <115565358+fadel-amar@users.noreply.github.com>
2025-03-27 23:25:44 +0100

    fix project
    fix project

---
+++ Models/RegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CesiZen_API.Models
+{
+    public class RegisterDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- fichier.py
+++
@@ -1,5 +0,0 @@
-for n in range(1,10):
-    print(" ")
-    print("la table de multiplication de  : ", n," est :")
-    for i in range(1,10):
-        print(n , " x ", i, " = ",i*n)
