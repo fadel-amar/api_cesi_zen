71e46f0ef57bc30cbe277c8ea3b2515adf03c940
fadel-amar <115565358+fadel-amar@users.noreply.github.com>
2025-04-12 19:00:07 +0200

    Create UserController.cs
    add controller User (add Get All user and GetUserById)

---
+++ Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace CesiZen_API.Controllers
+{
+
+
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context), "AppDbContext n'est pas défini");
+        }
+
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users =await _context.User
+                .Select(u => new { u.Id, u.Login, u.Email, u.CreatedAt, u.UpdatedAt, u.Banned, u.Disabled })
+                .ToListAsync();
+
+            if (users != null)
+            {
+                return Ok(users);
+            }
+
+            return NotFound(
+             new
+             {
+                 staus = 404,
+                 message = "Aucun utilisateur a été trouvé"
+             });
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.User
+                .Where(u => u.Id == id)
+                .Select(u => new { u.Id, u.Login, u.Email, u.CreatedAt, u.UpdatedAt, u.Banned, u.Disabled })
+                 .FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+
+            return NotFound(
+             new
+             {
+                 staus = 404,
+                 message = "Aucun utilisateur a été trouvé"
+             });
+
+        }
+    }
+
+}
